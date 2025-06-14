9483715a8420ad67b4e6620cedeba32638e5d7af
shankhwarsumit <shankhwarsumit117@gmail.com>
2025-06-14 18:00:08 +0530

    connecting to mongo

---
+++ db-connect.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+const url = `mongodb+srv://shankhwarsumit117:g911cCyAywMgjika@sumitdb.r1aimze.mongodb.net/`;
+
+const client = new MongoClient(url);
+
+const dbName = 'devTinder';
+
+async function run() {
+    try{
+       
+        await client.connect();
+        console.log('connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('usercollection');
+
+        // const findResult = await collection.find({}).toArray();
+        // console.log('found documents =>',findResult);
+
+        return "done";
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+run()
+ .then(console.log)
+ .catch(console.error)
+ .finally(()=> client.close());
